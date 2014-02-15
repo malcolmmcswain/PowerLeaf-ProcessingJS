e9b029c3178c79c01e92f6eb38a8eefbc8098588
Malcolm McSwain <mcswain1@pacbell.net>
2014-02-14 19:57:47 -0800

    Initial commit

---
+++ modules/modes/load.js
@@ -0,0 +1,16 @@
+var load = function() {
+    background(176, 207, 186);
+    noStroke();
+    fill(53, 138, 29);
+    textSize(30);
+    text("Power", 83, 145);
+    fill(130, 163, 131);
+    text("Leaf",167,146);
+    fill(99, 103, 181);
+    text("Framework",83,180);
+    image(getImage("avatars/leaf-grey"), 232, 85);
+    fill(148, 168, 149);
+    text("Loading content...", 95, 355);
+    fill(82, 82, 82);
+    text("We're getting things ready.", 25, 30);
+};
