fbc866ee0cca943bf28981aac34cc6c3e066c9fa
Malcolm McSwain <mcswain1@pacbell.net>
2014-02-15 14:47:00 -0800

    Initial commit

---
+++ framework/source/local-server.php
@@ -0,0 +1,16 @@
+<?php
+$script = false
+?>
+<script type="text/javascript" src="192.189.3.4"/>
+var server = get.user.data.ip
+var find = create.user.get.find
+find.server
+if (find.server = true) {
+     var serverstart = function(api, find){
+          var local = {
+               data: {
+                    this.x.y
+               }
+          }
+     }
+}
