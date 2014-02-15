caba8f355917188d750436e2d492e3a8ff701c76
Malcolm McSwain <mcswain1@pacbell.net>
2014-02-15 14:41:44 -0800

    Initial commit

---
+++ modules/switch.js
@@ -0,0 +1,14 @@
+switch(mode){
+    default:
+        load();
+    break;
+    case "editor":
+        editor();
+    break;
+    case "terminal":
+        terminal();
+    break;
+    case "load":
+        load();
+        
+}
